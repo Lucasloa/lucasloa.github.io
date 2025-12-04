9edd6b677f4ce11b9291743a89c9ccd871f98848
Lucasloa <lucasloa99@gmail.com>
2025-12-04 21:04:17 +0100

    vite change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/" // For a user page like lucasloa.github.io
+  base: '/lucasloa.github.io/' // <- must match your repo name
 });
