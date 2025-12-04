86223f9988d1a12f099e281a3ac3b3f028339066
Lucasloa <lucasloa99@gmail.com>
2025-12-04 18:12:27 +0100

    Setup gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // IMPORTANT for user.github.io pages
+  base: "/", // keep / for user pages like username.github.io
 })
